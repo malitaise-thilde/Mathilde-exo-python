51bd751a7bf3f2f33d0fd57f148bbefdf9953554
LITAISE Mathilde <mathilde.litaise@gmail.com>
2026-06-09 14:23:13 +0200

    exo 1 final

--- 01-basics/hello.py
+++ 01-basics/hello.py
@@ -5,3 +5,25 @@ Created on Tue Jun  9 13:53:19 2026
 @author: mathi
 """
 
+"""print("Bonjour le monde !")
+
+print("Python", "c'est parti")
+print(2026)
+print(10 + 5)
+
+prenom = "Mathilde"
+age = 20
+
+print(prenom)
+print(age)
+
+prenom = input("Quel est ton prénom ? ")
+print(prenom)
+
+print(f"Bonjour {prenom}, bienvenue dans la formation Python !")"""
+
+prenom = input("Quel est ton prénom ? ")
+age = int(input("Quel est ton âge ? "))
+age = age+10
+
+print(f"{prenom}, dans 10 ans tu auras {age} ans")
